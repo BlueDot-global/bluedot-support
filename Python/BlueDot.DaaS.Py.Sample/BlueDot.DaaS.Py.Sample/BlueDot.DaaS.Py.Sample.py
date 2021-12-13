ff1d2e35084a20da995d9d6805f7e36b258db743
BasamAhmad <bassam_asaleh@hotmail.com>
2021-12-13 14:00:04 -0500

    DAAS-350: Create python sample to consume notification from the BlueDot azure notifications service bus. (#6)
    Co-authored-by: Basam Ahmad <Basam.Ahmad@bluedot.global>

--- Python/BlueDot.DaaS.Py.Sample/BlueDot.DaaS.Py.Sample/BlueDot.DaaS.Py.Sample.py
+++ Python/BlueDot.DaaS.Py.Sample/BlueDot.DaaS.Py.Sample/BlueDot.DaaS.Py.Sample.py
@@ -1,9 +1,11 @@
-########### Python 3.7 #############
+########### Python 3.10.1 #############
 
 import requests
 from requests.structures import CaseInsensitiveDict
 
 try:
+ #Code block-1######################
+    #This is the first code block which, is a sample of calling BlueDot DaaS API
     #Gets location information for Japan from the lookup endpoint sample
     url = "https://developer.bluedot.global/daas/lookup/v2/api/locations/names/Japan/types/Country"
 
@@ -19,6 +21,27 @@ try:
     print('Response status code: ', resp.status_code)
     data = resp.content
     print('Response content: ', data)
+    
+ #Code block-2######################
+     #This is the second code block which, is a sample of listening to BlueDot DaaS push notification azure service bus
+    #For more information on using azure service bus, go to:
+    # https://docs.microsoft.com/en-us/python/api/overview/azure/servicebus-readme?view=azure-python
+
+    from azure.servicebus import ServiceBusClient, ServiceBusMessage
+
+    connstr = "{Your SERVICE_BUS_CONNECTION_STR}"
+    topic_name = "{Your SERVICE_BUS_TOPIC_NAME}"
+    subscription_name = "{Your SERVICE_BUS_SUBSCRIPTION_NAME}"
+
+    connstr = "Endpoint=sb://sb-bd-daas-notification-dev.servicebus.windows.net/;SharedAccessKeyName=Listen;SharedAccessKey=6Cu9wIFjAUT2g0calLFUt1S+zMWwqBSDmhlT5S0J/00=;EntityPath=sbt-alerts"
+    topic_name = "sbt-alerts"
+    subscription_name = "test-client"
+
+    with ServiceBusClient.from_connection_string(connstr) as client:
+        # If session_id is null here, will receive from the first available session.
+        with client.get_subscription_receiver(topic_name, subscription_name) as receiver:
+            for msg in receiver:
+                print(str(msg))
 
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
